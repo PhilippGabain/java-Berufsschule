21fd5147d1ec380291ca19c0c45c8921d289cba7
uig96002 <philipp.gabain.von.kries@continental.com>
2025-03-03 14:49:58 +0100

    LF5 files

---
+++ LF5/src/main/java/org/example/Vector.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Vector {
+    double[] values;
+    public Vector(double[] values){
+        this.values = values;
+    }
+
+    public double[] getValues() {
+        return values;
+    }
+
+    public void setValues(double[] values) {
+        this.values = values;
+    }
+}
